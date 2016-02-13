f96b1c12ec4363b7f64893172085474a775076bf
ubekishov <ulan.bekishov@coveros.com>
2016-02-12 20:30:52 -0500

    Jenkins DNS fix

--- src/com/coveros/selenium_hello_world/main.java
+++ src/com/coveros/selenium_hello_world/main.java
@@ -61,7 +61,7 @@ public class SeleniumMain {
             // ex.printStackTrace();
         }
 		DesiredCapabilities capability = DesiredCapabilities.firefox();
-		WebDriver driver = new RemoteWebDriver(new URL("http://ec2-54-210-131-171.compute-1.amazonaws.com:4444/wd/hub"), capability);
+		WebDriver driver = new RemoteWebDriver(new URL("http://ec2-54-165-9-170.compute-1.amazonaws.com:4444/wd/hub"), capability);
 		
 		// And now use this to visit the app
 		driver.get("http://" +DNS+":8080/hello-world-"+ appVersion + "/");
